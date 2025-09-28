85992c802c0dc8e8eb8fae804bab9769c5b6d3c7
bhavyakhatri2703 <khatribhavya2703@gmail.com>
2025-09-29 00:22:51 +0530

    server setup

---
+++ application/api/server.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"scheduler/database"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	Router *gin.Engine
+	DB     *database.Queries
+}
+
+func NewServer(db *database.Queries) *Server {
+	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	s := &Server{
+		Router: router,
+		DB:     db,
+	}
+	return s
+}
