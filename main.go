7b823de610c3b68a893413344abc3fdc3cdb0b38
bhavyakhatri2703 <khatribhavya2703@gmail.com>
2025-10-01 12:16:02 +0530

    initialize scheduler and worker in main

--- main.go
+++ main.go
@@ -6,29 +6,43 @@ import (
 	"log"
 	"scheduler/application/api"
 	"scheduler/database"
+	"scheduler/scheduler"
+	"scheduler/workers"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func ConnectDB() (*database.Queries, error) {
 	ctx := context.Background()
-	conn, err := pgx.Connect(ctx, "user=postgres password=postgres dbname=scheduler sslmode=disable")
+	pool, err := pgxpool.New(ctx, "user=postgres password=postgres dbname=scheduler sslmode=disable")
 
 	if err != nil {
 		return nil, err
 	}
 
-	db := database.New(conn)
+	db := database.New(pool)
 
 	return db, nil
 }
 
 func main() {
 
+	ctx := context.Background()
 	db, err := ConnectDB()
 	if err != nil {
 		log.Fatalf("Failed to connect to database %v", err)
 	}
+
+	taskChan := make(chan database.Task, 100)
+
+	schedulerEngine := scheduler.NewScheduler(db, taskChan)
+	schedulerCtx, schedulerCancel := context.WithCancel(ctx)
+	go schedulerEngine.StartScheduler(schedulerCtx)
+
+	workerPool := workers.NewWorkerPool(db, taskChan, 5)
+	workerCtx, workerCancel := context.WithCancel(ctx)
+	workerPool.Start(workerCtx)
+
 	server := api.NewServer(db)
 	server.SetRoutes()
 
@@ -36,4 +50,9 @@ func main() {
 	if err := server.Router.Run(":8080"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
+
+	schedulerCancel()
+
+	workerCancel()
+	workerPool.Stop()
 }
